6f51c5b4f6253fef97bdd6f376ffcec661fba839
Ronak Sakhuja <ronaksakhuja@Ronaks-MacBook-Air.local>
2022-02-16 17:40:53 -0800

    adding post data

--- server.py
+++ server.py
@@ -2,8 +2,24 @@ import requests
 import json
 from pydantic import BaseModel, Field
 from fastapi import Body, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5500",
+    "http://localhost:5500/cf.html",
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Names(BaseModel):
     mname:str
